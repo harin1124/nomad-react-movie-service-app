79d5ce898ec6c072929031bfc12930c11eec9430
harin1124 <lolrin1124@gmail.com>
2022-09-25 23:33:10 +0900

    use cleanup function

--- src/App.js
+++ src/App.js
@@ -1,31 +1,26 @@
 import {useState, useEffect} from "react";
 
+function Hello(){
+	function byeFn(){
+		console.log("bye :(");
+	}
+	function hiFn(){
+		console.log("created :)");
+		return byeFn; // cleanup function
+	}
+	useEffect(hiFn, []);
+	return <h1>Hello</h1>
+}
+
 function App() {
-	const [counter, setValue] = useState(0);
-	const [keyword, setKeyword] = useState("");
-	const onClick = () => setValue(prev => prev+1);
-	const onChange = (event) => setKeyword(event.target.value);
-	console.log("i run all the time");
-	useEffect(() => {
-		console.log("i run only once.");
-	}, []);
-	useEffect(() => {
-		console.log("i run when 'keyword' changes.");
-	}, [keyword]);
-	useEffect(() => {
-		console.log("i run when 'counter' changes.");
-	}, [counter]);
-	// 두 이펙트를 합칠 수 있음
-	useEffect(() => {
-		console.log("i run when keyword & counter changes.");
-	}, [keyword, counter])
+	const [showing, setShowing] = useState(false);
+	const onClick = () => setShowing((prev) => !prev);
 	return (
 		<div>
-			<input type="text" onChange={onChange} value={keyword} placeholder="Search here"/>
-			<h1>{counter}</h1>
-			<button onClick={onClick}>Click me</button>
+			{showing ? <Hello/> : null}
+			<button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
 		</div>
-	);
+	)
 }
 
 export default App;
