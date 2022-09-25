6bf08592aff50bbfc99c582c2ba6a21288d9e151
harin1124 <lolrin1124@gmail.com>
2022-09-25 22:56:30 +0900

    use useEffect & deps prop

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import {useState, useEffect} from "react";
 
 function App() {
 	const [counter, setValue] = useState(0);
+	const [keyword, setKeyword] = useState("");
 	const onClick = () => setValue(prev => prev+1);
-	// 코드는 state가 변할 때마다 매번 실행되는 console
+	const onChange = (event) => setKeyword(event.target.value);
 	console.log("i run all the time");
-	const iRunOnlyOnce = () => {
-		// 한 번만 실행되는 console
-		console.log("i run only once.");
-	}
-	useEffect(iRunOnlyOnce, []);
+	useEffect(() => {
+		console.log("CALL THE API...");
+	}, []);
+	// keyword가 변화할 때만 실행되도록, deps에 keyword를 넣어준다
+	// keyword가 바뀔 때만
+	useEffect(() => {
+		console.log("SEARCH FOR", keyword);
+	}, [keyword]);
 	return (
 		<div>
+			<input type="text" onChange={onChange} value={keyword} placeholder="Search here"/>
 			<h1>{counter}</h1>
 			<button onClick={onClick}>Click me</button>
 		</div>
