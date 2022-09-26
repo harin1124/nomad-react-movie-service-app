d36e57bba39e4edc3bb31c40bb329b441e9eafd7
harin1124 <lolrin1124@gmail.com>
2022-09-26 20:12:08 +0900

    mini todo app cretae & todo delete function create

--- src/App.js
+++ src/App.js
@@ -1,33 +1,43 @@
 import {useState, useEffect} from "react";
 
-function Hello(){
-	function byeFn(){
-		console.log("bye :(");
-	}
-	function hiFn(){
-		console.log("created :)");
-		return byeFn; // cleanup function
-	}
-	useEffect(() => {
-		console.log("hi :)");
-		return () => console.log("bye :(");
-	});
-	useEffect(function(){
-		console.log("hi :)");
-		return function(){
-			console.log("bye :(");
-		}
-	}, []);
-	return <h1>Hello</h1>
-}
-
 function App() {
-	const [showing, setShowing] = useState(false);
-	const onClick = () => setShowing((prev) => !prev);
+	const [todo, setTodo] = useState("");
+	const [todos, setTodos] = useState([]);
+	const onChange = (event) => setTodo(event.target.value);
+	const onSubmit = (event) => {
+		event.preventDefault();
+		if(todo === ""){
+			return;
+		}
+		// 기존 값을 가져와서 값을 넣어 새로운 array를 반환
+		setTodos((currentArray) => [todo, ...currentArray]);
+		setTodo("");
+	}
+
+	// [추가기능] 해당 투두 삭제 구현
+	const clickItemDelete = (event) => {
+		const idx = Number(event.target.parentElement.getAttribute("data-index"));
+		setTodos((currentArray) => currentArray.filter((item, index) => index !== idx));
+	}
 	return (
 		<div>
-			{showing ? <Hello/> : null}
-			<button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+			<h1>My To Dos ({todos.length})</h1>
+			<form onSubmit={onSubmit}>
+				<input
+					type="text"
+					onChange={onChange}
+					value={todo}
+					placeholder="Write your to do..."
+				/>
+				<button onClick={null}>Add To Do</button>
+			</form>
+			<hr/>
+			{todos.map((item, index) => (
+				<li key={index} data-index={index}>
+					<span style={{marginRight:"10px"}}>{item}</span>
+					<button onClick={clickItemDelete}>delete</button>
+				</li>
+			))}
 		</div>
 	)
 }
