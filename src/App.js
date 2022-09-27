ac3949fb5b734b43115f22d4bd24b958f3d92b7b
harin1124 <lolrin1124@gmail.com>
2022-09-27 13:05:38 +0900

    coin challenge success

--- src/App.js
+++ src/App.js
@@ -2,7 +2,21 @@ import {useState, useEffect} from "react";
 
 function App() {
 	const [loading, setLoading] = useState(true);
+	const [dollar, setDollar] = useState(0);
 	const [coins, setCoins] = useState([]); // 시작값은 빈 배열, 그것도 하지 않으면 undefined 로 밑에 렌더링에서 오류
+	const [coin, setCoin] = useState("");
+
+	const dollarChange = (event) => {
+		setDollar(event.target.value);
+	}
+	const coinChange = (event) => {
+		const val = event.target.value;
+		if(val === ""){
+			return;
+		}
+		setCoin(coins.find(item => item.id === event.target.value).id);
+	}
+
 	// 아무것도 주시하고 있지 않으면, 단 한 번만 작동 => 컴포넌트의 시작에서만 작동
 	useEffect(() => {
 		fetch("https://api.coinpaprika.com/v1/tickers")
@@ -18,12 +32,18 @@ function App() {
 			{loading ? (
 				<strong>Loading...</strong>
 			) : (
-				<select>
-					{coins.map((item) => (
-						<option key={item.id}>{item.name} ({item.symbol}): {item.quotes.USD.price} USD</option>
-					))}
-				</select>
-
+				<div>
+					<input type="number" value={dollar} onChange={dollarChange} style={{width:"378px", height:"40px", marginBottom:"10px", padding:"10px", boxSizing:"border-box"}} placeholder="보유한 달러를 입력하세요."/>
+					<br/>
+					<select onChange={coinChange} style={{width:"auto", height:"50px", padding:"10px"}}>
+						<option value="">코인 종류를 선택해주세요.</option>
+						{coins.map((item) => (
+							<option key={item.id} value={item.id}>{item.name} ({item.symbol}): {item.quotes.USD.price} 달러</option>
+						))}
+					</select>
+					<br/>
+					{dollar} 달러로 {coin !== "" ? coin : ""}을 {coin !== "" ? Math.floor(dollar / coins.find(item => item.id === coin).quotes.USD.price) : 0} 개 구매 가능합니다.
+				</div>
 			)}
 		</div>
 	);
